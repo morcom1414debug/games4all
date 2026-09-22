5c62280cef6d3cb276a4831a84f558d62ff32a8b
morcom1414debug <morcom1414@gmail.com>
2026-09-22 22:10:55 +0700

    j

--- Rally77/rally.js
+++ Rally77/rally.js
@@ -799,6 +799,7 @@ async function processAnswer(pId, selectedOptId) {
 async function endTurn() {
     const pKeys = gameState.playerOrder || Object.keys(gameState.players);
     const nextTurn = (gameState.turnIndex + 1) % pKeys.length;
+await delayAsync(2200);
     await update(ref(db), { 
         [`games/RallyThai/rooms/${currentRoomId}/turnIndex`]: nextTurn,
         [`games/RallyThai/rooms/${currentRoomId}/turnExecuting`]: false
