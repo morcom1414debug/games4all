73e1da85c19456c0877e845aa64dd38154f32036
morcom1414debug <morcom1414@gmail.com>
2026-09-23 21:19:36 +0700

    j

--- Rally77/rally.js
+++ Rally77/rally.js
@@ -692,7 +692,7 @@ async function executeTurnAsync(pId, isAuto = false) {
             // แจ้งให้ทุกคนทราบเพียงว่ากำลังตอบคำถาม (โดยไม่เห็นโจทย์) เดินถึงจังหวัดปกติเล่น box1.mp3
             await syncActionEmit(`${pData.name} กำลังตอบคำถามจากจังหวัด ${sp.name}`, ['box1.mp3']);
 
-await delayAsync(1400);
+await delayAsync(2000);
             const q = qList[Math.floor(Math.random() * qList.length)];
             
             // Shuffle options
