1aa3b698a233c7745fab75662dd23b86da33aee4
morcom1414debug <morcom1414@gmail.com>
2026-09-23 10:16:51 +0700

    j

--- Rally77/rally2.js
+++ Rally77/rally2.js
@@ -807,7 +807,6 @@ async function endTurn() {
     
     const pKeys = gameState.playerOrder || Object.keys(gameState.players);
     const nextTurn = (gameState.turnIndex + 1) % pKeys.length;
-    await delayAsync(2000); 
     await update(ref(db), { 
         [`games/RallyThai/rooms/${currentRoomId}/turnIndex`]: nextTurn,
         [`games/RallyThai/rooms/${currentRoomId}/turnExecuting`]: false
